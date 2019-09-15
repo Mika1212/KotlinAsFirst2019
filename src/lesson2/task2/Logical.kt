cfb2445584b5555d66587459af328dda886710e6
Mika <ilravgaz03@mail.ru>
2019-09-15 20:42:39 +0300

    get it started

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -42,7 +42,15 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    
+    var b = 0
+    if (year % 400 == 0 || year % 4 == 0 && year % 100 > 0) b = 29
+    else b = 28
+    return (when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        4, 6, 9, 11 -> 30
+        else -> b
+    }
+            )
 }
 
 /**
@@ -55,7 +63,14 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    return if (x1 == x2 && y1 == y2 && x2 + r2 >= x1 + r1 && y2 + r2 >= y1 + r1) true
+    else if (x1 > x2 && y1 > y2 && x2 + r2 >= x1 + r1 && y2 + r2 >= y1 + r1) true
+    else if (x1 < x2 && y1 < y2 && x2 - r2 <= x1 - r1 && y2 - r2 <= y1 - r1) true
+    else if (x1 < x2 && y1 > y2 && x2 - r2 <= x1 - r1 && y2 + r2 >= y1 + r1) true
+    else if (x1 > x2 && y1 < y2 && x2 + r2 >= x1 + r1 && y2 - r2 <= y1 - r1) true
+    else false
+}
 
 /**
  * Средняя
