d350c347117b6c74d9027e8136fae26cb9773895
Mika <ilravgaz03@mail.ru>
2019-09-15 21:07:42 +0300

    ready 1 and 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -20,7 +20,8 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
 fun isNumberHappy(number: Int): Boolean {
-    return if (number / 10 % 10 + number % 10 == number / 100 % 10 + number / 1000) true else false
+    return if (number / 10 % 10 + number % 10 == number / 100 % 10 + number / 1000) true
+    else false
 }
 
 /**
@@ -31,7 +32,8 @@ fun isNumberHappy(number: Int): Boolean {
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    return if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)) true else false
+    return if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)) true
+    else false
 }
 
 
